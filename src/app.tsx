a256572e1c63585ad7394c8fb63bc9904f9b968b
Saccharin01 <chousik01@gmail.com>
2024-07-31 03:45:54 +0900

    tailwind Css

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
-import React from "react";
 import  ReactDOM  from "react-dom/client";
 import Counter from "./useState";
-
+import TestEffect from "./useEffect";
+import TestStateEffect from "./hooks";
+import './style.css'
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
+const style = "bg-blue-500 text-white p-4 rounded"
+
 root.render(
-  <Counter/>
+<div className={style}>
+    <TestStateEffect/>
+  </div>
 )
